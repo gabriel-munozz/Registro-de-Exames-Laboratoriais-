383158a61e1f4461838cc689323e7ba96743f00b
Gabriel Muñoz <gabrielmachadomunoz@gmail.com>
2026-03-18 12:07:31 +0000

    main

--- Exames/Main.java
+++ Exames/Main.java
@@ -2,21 +2,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        FileReader arq = new FileReader("exames.txt");
-        BufferedReader linha = new BufferedReader(arq);
-        //Lê a linha de cabeçalho do TXT
-        String aux = linha.readLine();
+        //Construção do menu principal e chamada das operações
         
-        //Lê a primeira linha do TXT
-        aux = linha.readLine();
-        while(aux != null){
-            String[] dados = aux.split(",");
-            System.out.print("\nAbreviação: " + dados[0]);
-            System.out.print(" Nome do exame: " + dados[1]);
-            System.out.print(" Resultado: " + dados[2] + " dias");
-            aux = linha.readLine();
-        }
-        arq.close();
-
     }
 }
